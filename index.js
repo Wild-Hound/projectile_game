fd021dae8e907a06f1c039b5d7f19706c913af5b
Wild-Hound <wildhound404@gmail.com>
2024-05-04 03:25:57 +0600

    created player bullet

--- index.js
+++ index.js
@@ -17,6 +17,32 @@ class Player {
   }
 }
 
+class Projectile {
+  constructor(x, y, radius, color, velocity, ctx) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+    this.ctx = ctx;
+  }
+
+  draw() {
+    const ctx = this.ctx;
+
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+
+  update() {
+    this.draw();
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+  }
+}
+
 //scene objects
 const canvas = document.querySelector("canvas");
 canvas.width = innerWidth;
@@ -27,5 +53,29 @@ const centerX = canvas.width / 2;
 const centerY = canvas.height / 2;
 
 //player objects
-const player1 = new Player(centerX, centerY, 30, "blue", ctx);
-player1.draw();
+const player = new Player(centerX, centerY, 30, "blue", ctx);
+
+//player events
+addEventListener("click", (e) => {
+  const angle = Math.atan2(e.clientY - centerY, e.clientX - centerX);
+  const velocity = {
+    x: Math.cos(angle),
+    y: Math.sin(angle),
+  };
+
+  const projectile = new Projectile(centerX, centerY, 5, "red", velocity, ctx);
+  projectiles.push(projectile);
+});
+
+//animation
+const projectiles = [];
+const animate = () => {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  player.draw();
+  projectiles.forEach((projectile) => {
+    projectile.update();
+  });
+};
+
+animate();
