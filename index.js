523a22caf44f6d0e846d4ea4a86e0b4ce2058e1c
Wild-Hound <wildhound404@gmail.com>
2024-05-04 03:49:06 +0600

    spawning enemies randomly

--- index.js
+++ index.js
@@ -43,6 +43,32 @@ class Projectile {
   }
 }
 
+class Enemy {
+  constructor(x, y, radius, color, velocity, ctx) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+    this.ctx = ctx;
+  }
+
+  draw() {
+    const ctx = this.ctx;
+
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+
+  update() {
+    this.draw();
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+  }
+}
+
 //scene objects
 const canvas = document.querySelector("canvas");
 canvas.width = innerWidth;
@@ -67,15 +93,50 @@ addEventListener("click", (e) => {
   projectiles.push(projectile);
 });
 
+const spawnEnemy = () => {
+  setInterval(() => {
+    const radius = Math.random() * 20 + 10;
+
+    let x;
+    let y;
+    if (Math.random() < 0.5) {
+      x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+      y = Math.random() * canvas.height;
+    } else {
+      x = Math.random() * canvas.width;
+      y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
+    }
+
+    const color = "green";
+
+    const angle = Math.atan2(centerY - y, centerX - x);
+    const velocity = {
+      x: Math.cos(angle),
+      y: Math.sin(angle),
+    };
+
+    const enemy = new Enemy(x, y, radius, color, velocity, ctx);
+    enemies.push(enemy);
+  }, 1000);
+};
+
 //animation
 const projectiles = [];
+const enemies = [];
 const animate = () => {
   requestAnimationFrame(animate);
+
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   player.draw();
+
   projectiles.forEach((projectile) => {
     projectile.update();
+  });
+
+  enemies.forEach((enemy) => {
+    enemy.update();
   });
 };
 
 animate();
+spawnEnemy();
